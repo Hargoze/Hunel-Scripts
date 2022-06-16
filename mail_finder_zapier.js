dc125e341f39aee27c071055acf60f9a10e53464
Hargoze <francois.lecarpentier99@gmail.com>
2022-06-16 14:29:03 +0200

    zapier script can parse offer names and id

--- mail_finder_zapier.js
+++ mail_finder_zapier.js
@@ -1,26 +1,46 @@
 // this is wrapped in an `async` function
 // you can use await throughout the function
 
-function get_offer_id(data) {
-    var splited_balises = data.split("<");
-    var match = [];
+function get_offer_name(data) {
+    var splited_balises = data.split("<")
+    var match = []
 
     splited_balises.forEach(data => {
+        if (data.includes("subject")) {
+            //data = decodeURI(data)
+            data = decodeURIComponent(data)
+            data = data.replace(/ *\([^)]*\) */g, "");
+            match = data.split(/[:&]+/)[1]
+        }
+    });
+    try {
+        return match
+    } catch {
+        return ""
+    }
+}
 
-        if (data.includes("1f51ab68c607242a")) {
-            data.split("-").forEach(elem => {
-                if (elem.includes("Merci"))
-                    match.push(elem);
+
+function get_offer_id(data) {
+    var splited_balises = data.split("<")
+    var match = []
+
+    splited_balises.forEach(data => {
+        if (data.includes("subject")) {
+            //data = decodeURI(data)
+            data = decodeURIComponent(data)
+             data.split(/[\s-]+/).forEach(elem => {
+               if (/^(?=.*[a-zA-Z])(?=.*[0-9])/.test(elem) && elem.length === 16 && ! /^[%?]+$/.test(elem))
+                   match.push(elem)
             })
         }
     });
     try {
-        return match[1].toString().slice(0, 16);
+        return match[0]
     } catch {
-
+        return ""
     }
 }
-
 
 function remove_duplicates_safe(arr) {
     if (!arr)
@@ -38,7 +58,8 @@ function remove_duplicates_safe(arr) {
 }
 
 var decoded = Buffer.from(inputData.data, 'base64').toString()
-var id = get_offer_id(decoded);
+var offer_id = get_offer_id(decoded);
+var offer = get_offer_name(decoded)
 var emails = decoded.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi)
 var tmp = remove_duplicates_safe(emails)
 var result = []
@@ -50,4 +71,4 @@ tmp.forEach(elem => {
 
 var result_email = result[0]
 
-output = [{id, result_email}];
+output = [{offer_id, offer, result_email}];
